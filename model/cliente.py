b5bc67b4fb9b838a4bbc6e82641a0be1ca23b505
Santiago Mesa <thiagomesa21@gmail.com>
2024-07-23 18:54:48 -0500

    creando los modelos
    hemos creado los modelos para cliente y comida.

---
+++ model/cliente.py
@@ -0,0 +1,7 @@
+#cliente.py
+
+class Cliente:
+    def __init__(self, id=None, nombre=None, direccion=None):
+        self.id = id
+        self.nombre = nombre
+        self.direccion = direccion
---
+++ model/comida.py
@@ -0,0 +1,8 @@
+#comida.py
+
+class Comida:
+    def __init__(self, id=None, nombre=None, precio=None):
+        self.id = id
+        self.nombre = nombre
+        self.precio = precio
+
