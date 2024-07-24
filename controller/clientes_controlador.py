c3aad63c73cb5fa2104b07f5b80f3128cac22656
Santiago Mesa <thiagomesa21@gmail.com>
2024-07-23 20:20:47 -0500

    creando los controladores

---
+++ controller/clientes_controlador.py
@@ -0,0 +1,22 @@
+#clientes_controlador.py
+
+from vista.clientes_vista import ClientesVista
+from model.cliente import Cliente
+
+class ClientesControlador:
+    def __init__(self, cliente_repositorio):
+        self.cliente_repositorio = cliente_repositorio
+        self.vista = ClientesVista()
+
+    def listar(self):
+        clientes = self.cliente_repositorio.todos()
+        self.vista.mostrar(clientes)
+
+    def agregar(self):
+        nombre = self.vista.pedir('nombre')
+        direccion = self.vista.pedir('direccion')
+        cliente = Cliente(nombre=nombre, direccion=direccion)
+        self.cliente_repositorio.agregar(cliente)
+
+
+   
---
+++ controller/comidas_controlador.py
@@ -0,0 +1,19 @@
+#comidas_controlador.py
+
+from vista.comidas_vista import ComidasVista
+from model.comida import Comida
+
+class comidasControlador:
+    def __init__(self, comida_repositorio):
+        self.comida_repositorio = comida_repositorio
+        self.vista = ComidasVista()
+
+    def listar(self):
+        comidas = self.comida_repositorio.todos()
+        self.vista.mostrar(comidas)
+
+    def agregar(self):
+        nombre = self.vista.pedir('nombre')
+        precio = self.vista.pedir('precio')
+        comida = Comida(nombre=nombre, precio=precio)
+        self.comida_repositorio.agregar(comida)
